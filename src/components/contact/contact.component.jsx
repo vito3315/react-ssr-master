d259e515e6601703e3faa8d6824ca789c73953eb
vito3315 <marusiaqwerti@yandex.ru>
2021-04-25 13:56:54 +0400

    1

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -90,12 +90,12 @@ export function Contact() {
   
     return (
         <div class="section">
-          <div class="section__item">Item 1</div>
-          <div class="section__item">Item 2</div>
-          <div class="section__item">Item 3</div>
-          <div class="section__item">Item 4</div>
-          <div class="section__item">Item 5</div>
-        </div>
+  <div class="section__item bg-1">Item 1</div>
+  <div class="section__item bg-2">Item 2</div>
+  <div class="section__item bg-3">Item 3</div>
+  <div class="section__item bg-4">Item 4</div>
+  <div class="section__item bg-5">Item 5</div>
+</div>
     )
   
     /*return (
