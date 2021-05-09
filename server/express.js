a1446bc5170723d5d3cac3289ac19251fbf16a6d
vito3315 <marusiaqwerti@yandex.ru>
2021-05-09 13:01:09 +0400

    test

--- server/express.js
+++ server/express.js
@@ -38,7 +38,7 @@ app.use( '*', async ( req, res ) => {
 
         // get HTML string from the `App` component
         let appHTML = ReactDOMServer.renderToString(
-            <StaticRouter location={ req.originalUrl } context={ componentData }>
+            <StaticRouter location={ req } context={ componentData }>
                 <App />
             </StaticRouter>
         );
