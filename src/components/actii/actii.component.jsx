88d6fd5a14e9540fa0bed35543b42f3798a23449
vito3315 <marusiaqwerti@yandex.ru>
2021-05-09 13:57:52 +0400

    test

--- src/components/actii/actii.component.jsx
+++ src/components/actii/actii.component.jsx
@@ -74,7 +74,9 @@ export class Actii extends React.Component {
             if(response['status'] === 200){
                 var json = response['data'];
                 
-                //console.log( 'data', json )
+                console.log( 'data', json.page )
+                
+                return json;
                 
                 return {
                     title: json.page.title,
