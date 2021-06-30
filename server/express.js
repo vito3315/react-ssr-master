9a15c5d8e483783427ffd24b891b01f54e112f6f
vito3315 <marusiaqwerti@yandex.ru>
2021-06-30 21:45:09 +0400

    test

--- server/express.js
+++ server/express.js
@@ -165,7 +165,7 @@ app.use( '*', async ( req, res ) => {
         // set value of `initial_state` global variable
         indexHTML = indexHTML.replace(
             'var initial_state = null;',
-            `var initial_state = ${ JSON.stringify( req.originalUrl ) };`
+            `var initial_state = ${ JSON.stringify( city ) };`
         );
 
         // set header and status
