e2b5ecceb1fa7e10aa41e05745c67b56974566fc
vito3315 <marusiaqwerti@yandex.ru>
2021-06-30 21:22:34 +0400

    test

--- server/express.js
+++ server/express.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 })
 
 app.use("/",function (request, response) {
-    response.redirect("togliatti")
+    response.redirect("/togliatti")
 });
 
 // for any other requests, send `index.html` as a response
