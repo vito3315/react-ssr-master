b4b87ce6a9e3afa3950c69dda613622b98b923bd
vito3315 <marusiaqwerti@yandex.ru>
2021-07-07 21:40:52 +0400

    test

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -787,12 +787,9 @@ export class Home extends React.Component {
                         <Grid container spacing={2} style={{ margin: 0, padding: '0px 10px', paddingBottom: 20, flexWrap: 'wrap', width: '100%' }} className="MainItems mainContainer" >
                             {cat.items.map((it, k) => (
                                 <Grid item xs={12} sm={4} md={3} xl={3} key={k} style={{ padding: '10px 8px', display: 'flex'}}>
-                                    <Hidden xsDown>
+                                    
                                         <CardItem data={it} type={'pc'} openItem={this.openItemPC.bind(this)} />
-                                    </Hidden>
-                                    <Hidden smUp>
-                                        <CardItem data={it} type={'mobile'} openItem={this.openItem.bind(this)} />
-                                    </Hidden>
+                                    
                                 </Grid>
                             ))}
                         </Grid>
