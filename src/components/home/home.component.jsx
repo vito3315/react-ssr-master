21458ada2d9c092ab151b755eef8bc68f901ddbc
vito3315 <marusiaqwerti@yandex.ru>
2021-07-08 17:40:36 +0400

    tst

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -880,18 +880,22 @@ export class Home extends React.Component {
                         
                         
                         {itemsStore.getAllItemsCat().map((cat, key) => 
-                            <div key={key} name={"cat"+cat.main_id} id={"cat"+cat.id}>
-                                <Grid container spacing={2} style={{ margin: 0, padding: '0px 36px', flexWrap: 'wrap', width: '100%', paddingBottom: 40 }} className="MainItems mainContainer">
-                                    <Typography variant="h5" component="h3">{ cat.name }</Typography>
-                                </Grid>
-                                <Grid container spacing={2} style={{ margin: 0, padding: '0px 10px', paddingBottom: 20, flexWrap: 'wrap', width: '100%' }} className="MainItems mainContainer" >
-                                    {cat.items.map((it, k) => (
-                                        <Grid item xs={12} sm={4} md={3} xl={3} key={k} style={{ padding: '10px 8px', display: 'flex'}}>
-                                            <CardItemBot city={this.state.city_name} data={it} type={'pc'} openItem={this.openItemPC.bind(this)} />
-                                        </Grid>
-                                    ))}
-                                </Grid>
-                            </div>
+                            { mainLink == '' || mainLink == cat.main_link ?
+                                <div key={key} name={"cat"+cat.main_id} id={"cat"+cat.id}>
+                                    <Grid container spacing={2} style={{ margin: 0, padding: '0px 36px', flexWrap: 'wrap', width: '100%', paddingBottom: 40 }} className="MainItems mainContainer">
+                                        <Typography variant="h5" component="h3">{ cat.name }</Typography>
+                                    </Grid>
+                                    <Grid container spacing={2} style={{ margin: 0, padding: '0px 10px', paddingBottom: 20, flexWrap: 'wrap', width: '100%' }} className="MainItems mainContainer" >
+                                        {cat.items.map((it, k) => (
+                                            <Grid item xs={12} sm={4} md={3} xl={3} key={k} style={{ padding: '10px 8px', display: 'flex'}}>
+                                                <CardItemBot city={this.state.city_name} data={it} type={'pc'} openItem={this.openItemPC.bind(this)} />
+                                            </Grid>
+                                        ))}
+                                    </Grid>
+                                </div>
+                                    :
+                                null
+                            }
                         )}
                         
                         <Hidden smUp>
@@ -926,23 +930,27 @@ export class Home extends React.Component {
                         </Hidden>
                         
                         {itemsStore.getAllItemsCat().map((cat, key) => 
-                            <div key={key} name={"cat"+cat.main_id} id={"cat"+cat.id}>
-                                <Grid container spacing={2} style={{ margin: 0, padding: '0px 36px', flexWrap: 'wrap', width: '100%', paddingBottom: 40 }} className="MainItems mainContainer">
-                                    <Typography variant="h5" component="h3">{ cat.name }</Typography>
-                                </Grid>
-                                <Grid container spacing={2} style={{ margin: 0, padding: '0px 10px', paddingBottom: 20, flexWrap: 'wrap', width: '100%' }} className="MainItems mainContainer" >
-                                    {cat.items.map((it, k) => (
-                                        <Grid item xs={12} sm={4} md={3} xl={3} key={k} style={{ padding: '10px 8px', display: 'flex'}}>
-                                            <Hidden xsDown>
-                                                <CardItem data={it} type={'pc'} openItem={this.openItemPC.bind(this)} />
-                                            </Hidden>
-                                            <Hidden smUp>
-                                                <CardItem data={it} type={'mobile'} openItem={this.openItem.bind(this)} />
-                                            </Hidden>
-                                        </Grid>
-                                    ))}
-                                </Grid>
-                            </div>
+                            { mainLink == '' || mainLink == cat.main_link ?
+                                <div key={key} name={"cat"+cat.main_id} id={"cat"+cat.id}>
+                                    <Grid container spacing={2} style={{ margin: 0, padding: '0px 36px', flexWrap: 'wrap', width: '100%', paddingBottom: 40 }} className="MainItems mainContainer">
+                                        <Typography variant="h5" component="h3">{ cat.name }</Typography>
+                                    </Grid>
+                                    <Grid container spacing={2} style={{ margin: 0, padding: '0px 10px', paddingBottom: 20, flexWrap: 'wrap', width: '100%' }} className="MainItems mainContainer" >
+                                        {cat.items.map((it, k) => (
+                                            <Grid item xs={12} sm={4} md={3} xl={3} key={k} style={{ padding: '10px 8px', display: 'flex'}}>
+                                                <Hidden xsDown>
+                                                    <CardItem data={it} type={'pc'} openItem={this.openItemPC.bind(this)} />
+                                                </Hidden>
+                                                <Hidden smUp>
+                                                    <CardItem data={it} type={'mobile'} openItem={this.openItem.bind(this)} />
+                                                </Hidden>
+                                            </Grid>
+                                        ))}
+                                    </Grid>
+                                </div>
+                                    :
+                                null
+                            }
                         )}
                         
                         {this.state.openItem ?
