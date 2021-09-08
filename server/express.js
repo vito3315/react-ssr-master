e41d68e6b7319e62da88b1e004ec4e3f631fe65d
vito3315 <marusiaqwerti@yandex.ru>
2021-09-08 12:39:47 +0400

    test

--- server/express.js
+++ server/express.js
@@ -69,6 +69,8 @@ app.use( '*', async ( req, res ) => {
             componentData = await matchRoute.component.fetchData(req.originalUrl);
         }
 
+        console.log( 'componentData', componentData )
+        
         if( !componentData || componentData.st === false ){
             if( !componentData ){
                 res.status( 404 );
