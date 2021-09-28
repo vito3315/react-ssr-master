208b1113ea820257105b8851db2f8b3428eb8c59
vito3315 <marusiaqwerti@yandex.ru>
2021-09-28 14:04:19 +0400

    test

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -308,7 +308,7 @@ class CardItem extends React.Component {
                         <Typography className="CardInfoItem" component="p" onClick={ () => this.props.openItem(this.state.item.id)}>{this.state.item.tmp_desc}</Typography>
                         <div>
                             <div>
-                                <Typography gutterBottom className="CardPriceItem_old" variant="h5" component="span">{this.state.item.price} <Ruble width="20" viewBox="200 -200 400 600" /></Typography>
+                                <Typography gutterBottom className="CardPriceItem_old" variant="h5" component="span">{this.state.item.price} <Ruble width="20" viewBox="100 -100 800 300" /></Typography>
                                 <Typography gutterBottom className="CardPriceItem" variant="h5" component="span">{this.state.item.price} <Ruble width="20" viewBox="200 -200 400 600" /></Typography>
                             </div>
                             {this.state.count == 0 ?
