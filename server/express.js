59ab56444f36f6d98d1ee089470836b59f5755c7
vito3315 <marusiaqwerti@yandex.ru>
2021-12-07 21:22:53 +0400

    test

--- server/express.js
+++ server/express.js
@@ -55,6 +55,10 @@ app.use( '*', async ( req, res ) => {
     if( req.originalUrl.indexOf('b3ce5e32a5e236fccfae.ttf') ){
         express.static( path.resolve( __dirname, '../dist/b3ce5e32a5e236fccfae.ttf' ) );
     }
+
+    if( req.originalUrl.indexOf('FuturaFuturisC-Bold.ttf') ){
+        express.static( path.resolve( __dirname, '../dist/FuturaFuturisC-Bold.ttf' ) );
+    }
     
     //console.log( 'url', req )
     
