26e27ede715a25f08e300a1f7cd12c914183b53a
vito3315 <marusiaqwerti@yandex.ru>
2021-12-09 18:45:55 +0400

    test

--- src/components/cart/cart.component.jsx
+++ src/components/cart/cart.component.jsx
@@ -770,14 +770,20 @@ export class Cart extends React.Component {
         
         try{
 
-            this.setState({
-                isOpera: (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0,
-                isSafari: /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification)),
-                isFirefox: typeof InstallTrigger !== 'undefined',
-                isIE: /*@cc_on!@*/false || !!document.documentMode,
-                isEdge: !isIE && !!window.StyleMedia,
-                isChrome: !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)
-            })
+            // Opera 8.0+
+            const isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+            // Firefox 1.0+
+            const isFirefox = typeof InstallTrigger !== 'undefined';
+            // Safari 3.0+ "[object HTMLElementConstructor]"
+            const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+            // Internet Explorer 6-11
+            const isIE = /*@cc_on!@*/false || !!document.documentMode;
+            // Edge 20+
+            const isEdge = !isIE && !!window.StyleMedia;
+            // Chrome 1 - 71
+            const isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+            
 
             setTimeout( () => {
                 console.log( 'isOpera', isOpera )
