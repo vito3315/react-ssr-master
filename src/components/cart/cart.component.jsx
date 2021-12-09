b437bc5b025356c66422354154dd90541d1ca52b
vito3315 <marusiaqwerti@yandex.ru>
2021-12-09 15:36:29 +0400

    test

--- src/components/cart/cart.component.jsx
+++ src/components/cart/cart.component.jsx
@@ -71,6 +71,33 @@ import {Helmet} from "react-helmet";
 const queryString = require('query-string');
 import axios from 'axios';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getMessaging, getToken } from "firebase/messaging";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAK8l7m2URB6kFbBzC5iv67W34cuEzPKYc",
+  authDomain: "macro-thinker-288611.firebaseapp.com",
+  databaseURL: "https://macro-thinker-288611.firebaseio.com",
+  projectId: "macro-thinker-288611",
+  storageBucket: "macro-thinker-288611.appspot.com",
+  messagingSenderId: "989415800368",
+  appId: "1:989415800368:web:5baae6a54f5b8cb43177f5",
+  //measurementId: "${config.measurementId}"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const messaging = getMessaging();
+
 function get_city(path){
     
     path = path.split('/');
@@ -1946,8 +1973,21 @@ export class Cart extends React.Component {
     }
 
     testBTN(){
-        console.log( this.props.test )
-        //test123();
+        getToken(messaging, { vapidKey: 'BJmoVaG5ijS0CXc126Y47xmkjxv92stPrkQDfLql5hirvoWvAcy2N4xR1CPKVnCzUVai3ZqkzvVAjOyHGUWhogA' }).then((currentToken) => {
+            if (currentToken) {
+                console.log( currentToken )
+                localStorage.setItem('appToken', currentToken)
+              // Send the token to your server and update the UI if necessary
+              // ...
+            } else {
+              // Show permission request UI
+              console.log('No registration token available. Request permission to generate one.');
+              // ...
+            }
+          }).catch((err) => {
+            console.log('An error occurred while retrieving token. ', err);
+            // ...
+          });
     }
 
     render() {
