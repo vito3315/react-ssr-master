80041543af6bf88ad9b41e2dc2e4b966acf52aaa
vito3315 <marusiaqwerti@yandex.ru>
2021-12-10 09:51:43 +0400

    еуые

--- src/components/cart/cart.component.jsx
+++ src/components/cart/cart.component.jsx
@@ -2019,6 +2019,27 @@ export class Cart extends React.Component {
     }
 
     testBTN(){
+
+        if (!("Notification" in window)) {
+            alert("This browser does not support desktop notification");
+          }
+        
+          // Let's check whether notification permissions have already been granted
+          else if (Notification.permission === "granted") {
+            // If it's okay let's create a notification
+            var notification = new Notification("Hi there!");
+          }
+        
+          // Otherwise, we need to ask the user for permission
+          else if (Notification.permission !== "denied") {
+            Notification.requestPermission().then(function (permission) {
+              // If the user accepts, let's create a notification
+              if (permission === "granted") {
+                var notification = new Notification("Hi there!");
+              }
+            });
+          }
+
         const messaging = getMessaging();
 
         getToken(messaging, { vapidKey: 'BJmoVaG5ijS0CXc126Y47xmkjxv92stPrkQDfLql5hirvoWvAcy2N4xR1CPKVnCzUVai3ZqkzvVAjOyHGUWhogA' }).then((currentToken) => {
