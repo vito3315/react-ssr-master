d937b5f1e05f00430c894cf9e23d54a2b93e6940
vito3315 <marusiaqwerti@yandex.ru>
2022-01-12 15:45:43 +0400

    test

--- src/components/cart/cart.component.jsx
+++ src/components/cart/cart.component.jsx
@@ -2723,7 +2723,7 @@ export class Cart extends React.Component {
                             }
 
                             { parseInt( this.state.orderType ) == 0 ? null :
-                                <Typography variant="h5" component="span" className="orderCheckText"><FontAwesomeIcon icon={ faQrcode } /> Уважаемые посетители кафе "Жако", посадка в зал осуществляется только при наличии qr - кода и документа, удостоверяющего личность. <а href="https://pravo.samregion.ru/wp-content/uploads/sites/2/2021/11/pgso291.pdf" target="_blank" style={{ textDecoration: 'none', color: '#000' }}>Постановление губернатора Самарской области от 6.11.2021 г.</а></Typography>
+                                <Typography variant="h5" component="span" className="orderCheckText"><FontAwesomeIcon icon={ faQrcode } /> Уважаемые посетители кафе "Жако", посадка в зал осуществляется только при наличии qr - кода и документа, удостоверяющего личность. </Typography>
                             }
                             
                             { parseInt( this.state.orderType ) == 0 && parseInt(this.state.orderAddr.point_id) == 3 && false ?
