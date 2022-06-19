29f1b924628a4fd5f19e2821565072025ae26269
vito3315 <marusiaqwerti@yandex.ru>
2022-06-19 22:56:55 +0400

    test

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1218,15 +1218,31 @@ export class Home extends React.Component {
                     }
                     
                     {this.state.openItem ?
-                        <Dialog maxWidth={'xl'} width={'75%'} style={{ borderRadius: 50 }} onClose={this.handleClosePC.bind(this)} className="modalActii Item OpenItemPC" open={this.state.openModalPC}>
-                            <IconButton aria-label="close" style={{ position: 'absolute', top: -63, left: -119, zIndex: 1500 }} onClick={this.handleClosePC.bind(this)}>
-                                <FontAwesomeIcon icon={faTimes} style={{ width: '10%', height: 'auto', color: '#fff' }} />
-                            </IconButton>
-                            
-                            <MuiDialogContent className="modalActiiContent">
-                                <Item2 itemId={this.state.openItem.id} item={this.state.openItem} />
-                            </MuiDialogContent>
-                        </Dialog>
+
+                        <>
+                            <Hidden mdDown>
+                                <Dialog maxWidth={'md'} fullWidth={true} style={{ borderRadius: 50 }} onClose={this.handleClosePC.bind(this)} className="modalActii Item OpenItemPC" open={this.state.openModalPC}>
+                                    <IconButton aria-label="close" style={{ position: 'absolute', top: -63, left: -119, zIndex: 1500 }} onClick={this.handleClosePC.bind(this)}>
+                                        <FontAwesomeIcon icon={faTimes} style={{ width: '10%', height: 'auto', color: '#fff' }} />
+                                    </IconButton>
+                                    
+                                    <MuiDialogContent className="modalActiiContent">
+                                        <Item2 itemId={this.state.openItem.id} item={this.state.openItem} />
+                                    </MuiDialogContent>
+                                </Dialog>
+                            </Hidden>
+                            <Hidden mdUp>
+                                <Dialog maxWidth={'xl'} fullWidth={true} style={{ borderRadius: 50 }} onClose={this.handleClosePC.bind(this)} className="modalActii Item OpenItemPC" open={this.state.openModalPC}>
+                                    <IconButton aria-label="close" style={{ position: 'absolute', top: -63, left: -119, zIndex: 1500 }} onClick={this.handleClosePC.bind(this)}>
+                                        <FontAwesomeIcon icon={faTimes} style={{ width: '10%', height: 'auto', color: '#fff' }} />
+                                    </IconButton>
+                                    
+                                    <MuiDialogContent className="modalActiiContent">
+                                        <Item2 itemId={this.state.openItem.id} item={this.state.openItem} />
+                                    </MuiDialogContent>
+                                </Dialog>
+                            </Hidden>
+                        </>
                             :
                         null
                     }
