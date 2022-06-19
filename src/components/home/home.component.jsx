04e91a082ef164a9ec4d780349ccc576f6f60b05
vito3315 <marusiaqwerti@yandex.ru>
2022-06-19 20:44:35 +0400

    test

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1214,7 +1214,7 @@ export class Home extends React.Component {
                     }
                     
                     {this.state.openItem ?
-                        <Dialog maxWidth={'lg'} fullWidth={true} style={{ borderRadius: 50 }} onClose={this.handleClosePC.bind(this)} className="modalActii Item OpenItemPC" open={this.state.openModalPC}>
+                        <Dialog maxWidth={'xl'} fullWidth={true} style={{ borderRadius: 50 }} onClose={this.handleClosePC.bind(this)} className="modalActii Item OpenItemPC" open={this.state.openModalPC}>
                             <IconButton aria-label="close" style={{ position: 'absolute', top: -63, left: -119, zIndex: 1500 }} onClick={this.handleClosePC.bind(this)}>
                                 <FontAwesomeIcon icon={faTimes} style={{ width: '10%', height: 'auto', color: '#fff' }} />
                             </IconButton>
