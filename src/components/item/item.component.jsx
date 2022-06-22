ab6517c6973217f0a9129d6ab29903536e86a0eb
vito3315 <marusiaqwerti@yandex.ru>
2022-06-22 21:52:47 +0400

    test

--- src/components/item/item.component.jsx
+++ src/components/item/item.component.jsx
@@ -849,17 +849,17 @@ export class Item2 extends React.Component {
                         <Grid item style={{ width: '59%', marginLeft: '4%', marginRight: '4%', border: '1px solid red', marginTop: '2%', marginBottom: '2%' }}>
                             <picture>
                                 <source srcset={`
-                                    https://storage.yandexcloud.net/site-img/${img_name}_138x138.jpeg 138w, 
-                                    https://storage.yandexcloud.net/site-img/${img_name}_146x146.jpeg 146w,
-                                    https://storage.yandexcloud.net/site-img/${img_name}_183x183.jpeg 183w,
-                                    https://storage.yandexcloud.net/site-img/${img_name}_233x233.jpeg 233w,
-                                    https://storage.yandexcloud.net/site-img/${img_name}_292x292.jpeg 292w
-                                    https://storage.yandexcloud.net/site-img/${img_name}_366x366.jpeg 366w,
-                                    https://storage.yandexcloud.net/site-img/${img_name}_584x584.jpeg 584w,
-                                    https://storage.yandexcloud.net/site-img/${img_name}_760x760.jpeg 760w,
-                                    https://storage.yandexcloud.net/site-img/${img_name}_1875x1875.jpeg 1875w`} 
+                                    https://storage.yandexcloud.net/site-img/${img_name}138x138.jpeg 138w, 
+                                    https://storage.yandexcloud.net/site-img/${img_name}146x146.jpeg 146w,
+                                    https://storage.yandexcloud.net/site-img/${img_name}183x183.jpeg 183w,
+                                    https://storage.yandexcloud.net/site-img/${img_name}233x233.jpeg 233w,
+                                    https://storage.yandexcloud.net/site-img/${img_name}292x292.jpeg 292w
+                                    https://storage.yandexcloud.net/site-img/${img_name}366x366.jpeg 366w,
+                                    https://storage.yandexcloud.net/site-img/${img_name}584x584.jpeg 584w,
+                                    https://storage.yandexcloud.net/site-img/${img_name}760x760.jpeg 760w,
+                                    https://storage.yandexcloud.net/site-img/${img_name}1875x1875.jpeg 1875w`} 
                                     sizes="(max-width=1439px) 233px, (max-width=1279px) 218px, 292px" />
-                                <img alt={this.state.item.name} title={this.state.item.name} class="img" src={`https://storage.yandexcloud.net/site-img/${img_name}_138x138.jpeg`} />
+                                <img alt={this.state.item.name} title={this.state.item.name} class="img" src={`https://storage.yandexcloud.net/site-img/${img_name}138x138.jpeg`} />
                             </picture>
                         </Grid>
                         <Grid item style={{ width: '29%', border: '1px solid red', marginTop: '2%', marginBottom: '2%', paddingTop: '8%' }}>
