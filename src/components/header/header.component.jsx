74a2001ab9b0252a3a4c98eef32ad0406b3f0f9a
vito3315 <marusiaqwerti@yandex.ru>
2022-07-12 22:48:28 +0400

    1111

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -362,12 +362,12 @@ class SimplePopover extends React.Component{
                                             </td>
                                             <td>
                                                 <ButtonGroup disableElevation={true} disableRipple={true} variant="contained" style={{ width: '100%' }}>
-                                                    <div variant="contained" className='ModalItemButtonCart OPEN' style={{ width: '100%', height: 28, borderRadius: 40, border: '1px solid #F9BC23', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                                                        <button style={{ width: 28, height: 28, padding: 0, marginLeft: 13, color: '#525252', fontSize: '0.875rem', fontFamily: 'Roboto', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'transparent', border: 'transparent' }} onClick={this.minus.bind(this)}>–</button>
+                                                    <div variant="contained" className='ModalItemButtonCart OPEN' style={{ width: 118, height: 38, borderRadius: 40, border: '1px solid #F9BC23', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                                        <button style={{ width: 23, height: 23, padding: 0, marginLeft: 10, color: '#525252', fontSize: '0.875rem', fontFamily: 'Roboto', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#F9BC23', border: '1px solid #F9BC23', borderRadius: 60 }} onClick={this.minus.bind(this)}>–</button>
                                                         <div>
                                                             <span style={{ fontSize: '0.875rem', fontFamily: 'Roboto', fontWeight: 500, color: '#525252' }}>{item.count}</span>
                                                         </div>
-                                                        <button style={{ width: 28, height: 28, padding: 0, marginRight: 10, color: '#525252', fontSize: '0.875rem', fontFamily: 'Roboto', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'transparent', border: 'transparent' }} onClick={this.add.bind(this)}>+</button>
+                                                        <button style={{ width: 28, height: 28, padding: 0, marginRight: 6, color: '#525252', fontSize: '0.875rem', fontFamily: 'Roboto', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#F9BC23', border: '1px solid #F9BC23', borderRadius: 60 }} onClick={this.add.bind(this)}>+</button>
                                                     </div>
                                                 </ButtonGroup>
                                             </td>
@@ -400,12 +400,12 @@ class SimplePopover extends React.Component{
                                             </td>
                                             <td>
                                                 <ButtonGroup disableElevation={true} disableRipple={true} variant="contained" style={{ width: '100%' }}>
-                                                    <div variant="contained" className='ModalItemButtonCart OPEN' style={{ width: '100%', height: 28, borderRadius: 40, border: '1px solid #F9BC23', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                                                        <button style={{ width: 28, height: 28, padding: 0, marginLeft: 13, color: '#525252', fontSize: '0.875rem', fontFamily: 'Roboto', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'transparent', border: 'transparent' }} onClick={this.minus.bind(this)}>–</button>
+                                                    <div variant="contained" className='ModalItemButtonCart OPEN' style={{ width: 118, height: 38, borderRadius: 40, border: '1px solid #F9BC23', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                                        <button style={{ width: 23, height: 23, padding: 0, marginLeft: 10, color: '#525252', fontSize: '0.875rem', fontFamily: 'Roboto', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#F9BC23', border: '1px solid #F9BC23', borderRadius: 60 }} onClick={this.minus.bind(this)}>–</button>
                                                         <div>
                                                             <span style={{ fontSize: '0.875rem', fontFamily: 'Roboto', fontWeight: 500, color: '#525252' }}>{item.count}</span>
                                                         </div>
-                                                        <button style={{ width: 28, height: 28, padding: 0, marginRight: 10, color: '#525252', fontSize: '0.875rem', fontFamily: 'Roboto', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'transparent', border: 'transparent' }} onClick={this.add.bind(this)}>+</button>
+                                                        <button style={{ width: 28, height: 28, padding: 0, marginRight: 6, color: '#525252', fontSize: '0.875rem', fontFamily: 'Roboto', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#F9BC23', border: '1px solid #F9BC23', borderRadius: 60 }} onClick={this.add.bind(this)}>+</button>
                                                     </div>
                                                 </ButtonGroup>
                                             </td>
