2db95ce1c768fedc49df0bef080c8c0d7d5ca99b
vito3315 <marusiaqwerti@yandex.ru>
2022-07-14 22:47:31 +0400

    111

--- src/components/item/item.component.jsx
+++ src/components/item/item.component.jsx
@@ -29,6 +29,8 @@ import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Tooltip from '@material-ui/core/Tooltip';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 
+import { ActionsCartButton, ActionsCartButtonStart, IconRuble } from '../../stores/elements';
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics, logEvent } from "firebase/analytics";
 
@@ -109,18 +111,6 @@ function IconInfoBlack() {
         ></path>
         <circle cx="40" cy="22.666" r="4" fill="#fff"></circle>
       </svg>
-    );
-}
-
-function IconRuble(props) {
-    return (
-        <svg
-            style={ props.style }
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="-1 0 24 24"
-            >
-            <path d="M17.778 7.449a3.628 3.628 0 00-1.107-2.761l-.001-.001a4.03 4.03 0 00-2.923-1.057h.009-5.454v7.636h5.454a4.032 4.032 0 002.917-1.06l-.003.003a3.633 3.633 0 001.108-2.768v.007zm4.04 0a7.11 7.11 0 01-2.158 5.368l-.002.002a7.65 7.65 0 01-5.581 2.08h.015-5.795v2.011H16.926c.29 0 .525.235.525.525v.022-.001 2.203c0 .29-.235.525-.525.525h-.022.001-8.608v3.291a.537.537 0 01-.537.528H4.886a.525.525 0 01-.525-.525v-.022.001-3.273H.522a.525.525 0 01-.525-.525v-.022.001-2.182-.021c0-.29.235-.525.525-.525h.022-.001 3.818v-2.011H.522a.525.525 0 01-.525-.525v-.022.001-2.54-.006a.537.537 0 01.528-.537h.019-.001 3.818V.55.529c0-.29.235-.525.525-.525h.022-.001 9.187a7.653 7.653 0 015.57 2.084l-.004-.004a7.11 7.11 0 012.157 5.378v-.013z"></path>
-        </svg>
     );
 }
 
@@ -1206,23 +1196,9 @@ export class Item2 extends React.Component {
                             <Typography variant="h5" component="span" style={{ marginBottom: 20, minHeight: 200 }} className='ModalItemDesc'>{desc}</Typography>
                             
                             { this.state.count == 0 ?
-                                <ButtonGroup disableElevation={true} disableRipple={true} variant="contained" style={{ position: 'absolute', bottom: 0 }}>
-                                    <Button variant="contained" className='ModalItemButtonCart' style={{ width: 300, height: 60, borderRadius: 40, border: '2px solid #F9BC23' }} onClick={this.add.bind(this)}>
-                                        <span style={{ fontSize: '1.25rem', textTransform: 'initial', fontFamily: 'Roboto', color: '#525252', fontWeight: 700 }}>В корзину за { new Intl.NumberFormat('ru-RU').format(this.state.item.price)}</span>
-                                        <IconRuble style={{ width: 16, height: 16, fill: '#525252', marginLeft: 5 }} />
-                                    </Button>
-                                </ButtonGroup>
+                                <ActionsCartButtonStart price={this.state.item.price} add={this.add.bind(this)} />
                                     :
-                                <ButtonGroup disableElevation={true} disableRipple={true} variant="contained" style={{ position: 'absolute', bottom: 0 }}>
-                                    <div variant="contained" className='ModalItemButtonCart OPEN' style={{ width: 296, height: 56, borderRadius: 40, border: '2px solid #F9BC23', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                                        <button style={{ width: 30, height: 30, borderRadius: 40, padding: 0, marginLeft: 13, border: '1px solid #F9BC23', backgroundColor: '#F9BC23', color: '#525252', fontSize: '1.5rem', fontFamily: 'Roboto', fontWeight: 300, display: 'flex', justifyContent: 'center', alignItems: 'center' }} onClick={this.minus.bind(this)}>–</button>
-                                        <div>
-                                            <span style={{ fontSize: '1.25rem', textTransform: 'initial', fontFamily: 'Roboto', color: '#525252', fontWeight: 700 }}>{this.state.count} шт. на { new Intl.NumberFormat('ru-RU').format( parseInt(this.state.item.price) * parseInt(this.state.count) )}</span>
-                                            <IconRuble style={{ width: 16, height: 16, fill: '#525252', marginLeft: 5 }} />
-                                        </div>
-                                        <button style={{ width: 40, height: 40, borderRadius: 40, padding: 0, marginRight: 10, border: '1px solid #F9BC23', backgroundColor: '#F9BC23', color: '#525252', fontSize: '2rem', fontFamily: 'Roboto', fontWeight: 300, display: 'flex', justifyContent: 'center', alignItems: 'center' }} onClick={this.add.bind(this)}>+</button>
-                                    </div>
-                                </ButtonGroup>
+                                <ActionsCartButton count={this.state.count} price={this.state.item.price} item_id={this.state.item.id} minus={this.minus.bind(this)} add={this.add.bind(this)} />
                             }
                         </Grid>
                     </Grid>
