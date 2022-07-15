6b4aca05462425ca4af84aa5e931d6e1a3437a09
vito3315 <marusiaqwerti@yandex.ru>
2022-07-15 14:17:55 +0400

    111

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -571,7 +571,7 @@ class CardItem extends React.Component {
                                     </Button>
                                 </ButtonGroup>
                                     :
-                                <ButtonGroup disableElevation={true} disableRipple={true} variant="contained">
+                                <ButtonGroup disableElevation={true} disableRipple={true} variant="contained" style={{ width: '100%' }}>
                                     <div variant="contained" className='ModalItemButtonCart OPEN' style={{ width: '100%', height: 28, borderRadius: 40, border: '1px solid #F9BC23', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                                         <button style={{ width: 28, height: 28, padding: 0, marginLeft: 13, color: '#525252', fontSize: '0.875rem', fontFamily: 'Roboto', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'transparent', border: 'transparent' }} onClick={this.minus.bind(this)}>–</button>
                                         <div>
