f32a8c2d9ecd8db1a90bbefa14d73f6d3f748609
vito3315 <marusiaqwerti@yandex.ru>
2022-07-17 22:06:17 +0400

    111

--- src/components/item/item.component.jsx
+++ src/components/item/item.component.jsx
@@ -60,8 +60,8 @@ const HtmlTooltip = styled(({ className, ...props }) => (
     <Tooltip {...props} classes={{ popper: className }} />
   ))(({ theme }) => ({
     [`& .${tooltipClasses.tooltip}`]: {
-      backgroundColor: '#f5f5f9',
-      color: 'rgba(0, 0, 0, 0.87)',
+      //backgroundColor: '#f5f5f9',
+      //color: 'rgba(0, 0, 0, 0.87)',
       maxWidth: 220,
       fontSize: theme.typography.pxToRem(12),
       border: '1px solid #dadde9',
