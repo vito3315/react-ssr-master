1091be08edbf5453deb3a204e32b565c1b94cc19
vito3315 <marusiaqwerti@yandex.ru>
2022-07-22 10:15:48 +0400

    111

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -52,6 +52,87 @@ import { faUtensils, faUser, faGift } from '@fortawesome/free-solid-svg-icons'
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 
+import Modal from '@mui/material/Modal';
+// web.cjs is required for IE11 support
+import { useSpring, animated } from '@react-spring/web';
+
+const Fade = React.forwardRef(function Fade(props, ref) {
+    const { in: open, children, onEnter, onExited, ...other } = props;
+    const style = useSpring({
+      from: { opacity: 0 },
+      to: { opacity: open ? 1 : 0 },
+      onStart: () => {
+        if (open && onEnter) {
+          onEnter();
+        }
+      },
+      onRest: () => {
+        if (!open && onExited) {
+          onExited();
+        }
+      },
+    });
+  
+    return (
+      <animated.div ref={ref} style={style} {...other}>
+        {children}
+      </animated.div>
+    );
+});
+
+Fade.propTypes = {
+    children: PropTypes.element,
+    in: PropTypes.bool.isRequired,
+    onEnter: PropTypes.func,
+    onExited: PropTypes.func,
+};
+  
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+function SpringModal() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+  
+    return (
+      <div>
+        <Button onClick={handleOpen}>Open modal</Button>
+        <Modal
+          aria-labelledby="spring-modal-title"
+          aria-describedby="spring-modal-description"
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <Box sx={style}>
+              <Typography id="spring-modal-title" variant="h6" component="h2">
+                Text in a modal
+              </Typography>
+              <Typography id="spring-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </Box>
+          </Fade>
+        </Modal>
+      </div>
+    );
+  }
+
 class CustomBottomNavigation extends React.Component{
     constructor(props) {
         super(props);
@@ -1699,6 +1780,7 @@ export class Header extends React.Component {
                 </Dialog>
 
 
+                <SpringModal />
 
 
                 <Dialog
