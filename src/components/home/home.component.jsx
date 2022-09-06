b9a45fe6c33eee607c679645dc7719a2cacb6f21
vito3315 <marusiaqwerti@yandex.ru>
2022-09-06 22:29:39 +0400

    111

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -174,41 +174,25 @@ function ItemHookAdaptive(props) {
 
     console.log( 'device', device, props.type )
 
-    if( props.type == 'bot' ){
-        if( device == 'desktop' ){
-            console.log( 'render pc bot' )
-            return (
-                <Grid item className='_PC_' xs={12} sm={6} md={4} lg={3} xl={3} sx={{ display: 'flex' }} style={{ padding: '30px 16px', width: '100%' }}>
-                    <CardItemBotNew data={props.data} type={'pc'} openItem={props.openItemPC} />
-                </Grid>
-            )
-        }
     
-        console.log( 'render mobile bot' )
+    if( device === 'mobile' ){
+        
+        console.log( 'render mobile user' )
         return (
             <Grid item className='_mobile_' xs={12} sm={6} md={4} lg={3} xl={3} sx={{ display: 'flex' }} style={{ padding: '10px 0px', borderBottom: props.isLast ? 'none' : '1px solid rgba(27, 27, 31, 0.1)' }}>
-                <CardItemBotNew data={props.data} type={'mobile'} openItem={props.openItemMobile} />
+                <CardItem data={props.data} type={'mobile'} openItem={props.openItemMobile} />
             </Grid>
-        )
-    }else{
-        if( device === 'mobile' ){
             
-            console.log( 'render mobile user' )
-            return (
-                <Grid item className='_mobile_' xs={12} sm={6} md={4} lg={3} xl={3} sx={{ display: 'flex' }} style={{ padding: '10px 0px', borderBottom: props.isLast ? 'none' : '1px solid rgba(27, 27, 31, 0.1)' }}>
-                    <CardItem data={props.data} type={'mobile'} openItem={props.openItemMobile} />
-                </Grid>
-                
-            )
-        }
-    
-        console.log( 'render pc user' )
-        return (
-            <Grid item className='_PC_' xs={12} sm={6} md={4} lg={3} xl={3} sx={{ display: 'flex' }} style={{ padding: '30px 16px', width: '100%' }}>
-                <CardItem data={props.data} type={'pc'} openItem={props.openItemPC} />
-            </Grid>
         )
     }
+
+    console.log( 'render pc user' )
+    return (
+        <Grid item className='_PC_' xs={12} sm={6} md={4} lg={3} xl={3} sx={{ display: 'flex' }} style={{ padding: '30px 16px', width: '100%' }}>
+            <CardItem data={props.data} type={'pc'} openItem={props.openItemPC} />
+        </Grid>
+    )
+    
 }
 
 class CardItem extends React.Component {
