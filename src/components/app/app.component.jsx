8db1197d696067721d9fe0a3533cc801c719fcb3
vito3315 <marusiaqwerti@yandex.ru>
2023-11-24 14:08:01 +0400

    111

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -261,7 +261,7 @@ class StickyFooter extends React.Component{
                         null
                     }
 
-                    <Grid item lg={12} md={12} sm={12} xl={12} xs={12} className="copy">
+                    <Grid item lg={12} md={12} sm={12} xl={12} xs={12} className="copy" style={{ marginTop: 50 }}>
                         <Typography variant="body1" component="span">2017 - {new Date().getFullYear()} © ООО «Мистер Жако», ИНН: 6321390811</Typography>
                     </Grid>
                 </Grid>
