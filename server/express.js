faa47df556359a9eace0b21877d8a45a4e53baa1
vito3315 <marusiaqwerti@yandex.ru>
2024-02-20 11:41:05 +0400

    111

--- server/express.js
+++ server/express.js
@@ -36,15 +36,24 @@ app.get( /\.(eot|ttf|woff|woff2)$/, express.static( path.resolve( __dirname, '..
 
 //app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    if (req.path.slice(-1) === '/' && req.path.length > 1) {
-      const query = req.url.slice(req.path.length)
-      const safepath = req.path.slice(0, -1).replace(/\/+/g, '/')
-      res.redirect(301, safepath + query)
-    } else {
-      next()
-    }
-  })
+app.use(function(req, res, next){
+    // Find the query string
+    var qsi = req.originalUrl.indexOf('?');
+    // Get the path
+    var path = req.originalUrl;
+    if(qsi > -1) path = path.substr(0, qsi);
+    // Continue if the path is good or it's a static resource
+    if(path.substr(-1) === '/' || ~path.indexOf('.')) return next();
+    // Save just the query string
+    var qs = '';
+    if(qsi > -1) qs = req.originalUrl.substr(qsi);
+    // Save redirect path
+    var redirect = path + '/' + qs;
+    // Redirect client
+    res.redirect(301, redirect);
+
+    //console.log('301 redirected ' + req.originalUrl + ' to ' + redirect);
+});
 
 /*app.use((req, res, next) => {
     const test = /\?[^]*\//.test(req.url);
